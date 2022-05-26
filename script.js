63216b96e057e12ab952f44c26c0b2242ba1c9ec
E-Albert <magicspoon123@gmail.com>
2022-05-26 11:51:50 -0400

    Added functioning mole

---
+++ script.js
@@ -0,0 +1,37 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+
+function randomSquare() {
+    squares.forEach(square => 
+    {
+        square.classList.remove('mole')
+    })
+
+    let randomPosition = squares[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+    
+    hitPosition = randomPosition.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+        }
+    })
+})
+
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
